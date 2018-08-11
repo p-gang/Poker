3df8d3b0b181ba0fcc4fd41a6ae067839ca2fb6a
Vladius25 <vkorol2509@icloud.com>
2018-08-11 19:07:07 +0500

    Fixed

--- src/gui.py
+++ src/gui.py
@@ -1,25 +1,17 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QMainWindow
 
 
-class App(QWidget):
+class PokerWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.title = 'PyQt5 simple window - pythonspot.com'
-        self.left = 10
-        self.top = 10
+
+        self.title = 'Poker'
         self.width = 640
         self.height = 480
+
         self.initUI()
 
     def initUI(self):
         self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
-        self.show()
-
-
-
-app = QApplication(sys.argv)
-ex = App()
-sys.exit(app.exec_())
+        self.showFullScreen()

8130e96693b47c649ee53d552657ab52ab1a816e
WhatislovePerm <vto59@mail.ru>
2018-08-16 14:08:15 +0500

    Names

--- src/game.py
+++ src/game.py
@@ -4,11 +4,15 @@ from src.player import Player
 
 class Game:
     def __init__(self):
-        player1 = Player(0, 'Kevin')
+        player1 = Player(0, 'Winston')
         player1.small_blind = True
         player2 = Player(1, 'Adolf')
         player2.big_blind = True
-        self.table = Table([player1, player2])
+        player3 = Player(2, 'Franklin')
+        player4 = Player(3, 'Joseph')
+        player5 = Player(4, 'Benito')
+
+        self.table = Table([player1, player2, player3, player4, player5])
         self.is_dead = False
 
 

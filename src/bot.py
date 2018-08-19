d7819cfb7b5820bd13580d42b100781e3e674a6c
WhatislovePerm <vto59@mail.ru>
2018-08-19 12:58:06 +0500

    bot done

--- src/bot.py
+++ src/bot.py
@@ -1,5 +1,5 @@
 from src.player import Player
-
+from src.table import Table
 
 class bot(Player):
 
@@ -33,5 +33,37 @@ class bot(Player):
         else:
             return 5
 
-    def marking_situation(self, hand, bank):
+    def get_combination(self):
+        return self.combination
+
+    def set_combination(self, combination):
+        self.combination = combination
+
+    def bot_decision(self, table):
+        var = self.check_cards()
+        if var < 3:
+            if table.bet == 0:
+                self.bet(table, var)
+            else:
+                self.cards(table)
+        else:
+            if table.bet == 0:
+                self.call(table)
+            else:
+                self.fold(table)
+
+    def check(self):
         pass
+
+    def fold(self, table):
+        table.players.remove(self)
+
+    def call(self, table):
+        table.bank += table.bet
+        self.money -= table.bet
+
+    def bet(self, table, bet):
+        table.bank += bet
+        self.money -= bet
+        table.bet += bet
+
--- src/game.py
+++ src/game.py
@@ -1,23 +1,24 @@
 from src.table import Table
 from src.player import Player
-
+from src.bot import bot
 
 class Game:
     def __init__(self):
         player1 = Player(0, 'Winston')
         player1.small_blind = True
-        player2 = Player(1, 'Adolf')
+        player2 = bot(1, 'Adolf')
         player2.big_blind = True
-        player3 = Player(2, 'Franklin')
-        player4 = Player(3, 'Joseph')
-        player5 = Player(4, 'Benito')
+        player3 = (2, 'Franklin')
+        player4 = bot(3, 'Joseph')
+        player5 = bot(4, 'Benito')
 
+        self.is_starting = True
         self.table = Table([player1, player2, player3, player4, player5])
         self.is_dead = False
 
 
     def start_game(self):
-        self.status = "game_start"
+        self.status = "player_turn"
         # ans = self.table.change_blind()
         # self.table.take_blind(ans[0], ans[1])
         # self.table.take_cards()

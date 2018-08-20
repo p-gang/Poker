a03a186eab6c0e4b8a58d45ff33843a654728db7
Vladius25 <vkorol2509@icloud.com>
2018-08-20 11:19:22 +0500

    Coord fix

--- src/gui/object.py
+++ src/gui/object.py
@@ -1,9 +1,14 @@
+import pygame
+
 from pygame.rect import Rect
 
 
 class GameObject:
     def __init__(self, x, y, w, h):
-        self.bounds = Rect(x, y, w, h)
+        info_obj = pygame.display.Info()
+        kx = 1920 / info_obj.current_w
+        ky = 1080 / info_obj.current_h
+        self.bounds = Rect(x /kx, y /ky, w, h)
 
     def draw(self, screen):
         pass
